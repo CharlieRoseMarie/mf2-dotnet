fddd93bf7845ed6273ba3f165de8d60c897a0b2b
Charlotte Allen <CharlotteRMAllen@gmail.com>
2019-12-02 01:12:07 -0800

    Cleanup structure and add serialization information

--- Mf2-DotNet/Mf2-DotNet/src/Structure/IMf2Struct.cs
+++ Mf2-DotNet/Mf2-DotNet/src/Structure/IMf2Struct.cs
@@ -1,13 +1,19 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Mf2_DotNet.Structure
 {
     public interface IMf2Struct
     {
-        IEnumerable<IMf2Struct> Items { get; set; }
+        [JsonProperty(PropertyName = "items")] IEnumerable<IMf2Struct> Items { get; set; }
+
         string Value { get; set; }
-        IEnumerable<string> Rels { get; set; }
-        IEnumerable<string> RelUrls { get; set; }
+
+        [JsonProperty(PropertyName = "rels")] ISet<string> Rels { get; set; }
+
+        [JsonProperty(PropertyName = "rel-urls")]
+        ISet<string> RelUrls { get; set; }
+
         void Accept(IMf2Visitor visitor);
     }
 }
